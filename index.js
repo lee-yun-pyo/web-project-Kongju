a19ef81caeacf96640024569379d1ad420e4e2a3
lee-yun-pyo <41375597+lee-yun-pyo@users.noreply.github.com>
2022-06-07 21:08:57 +0900

    footer 코드 추가

--- index.js
+++ index.js
@@ -12,3 +12,22 @@ function startTimer() {
 }
 
 originalImg.addEventListener("click", startTimer);
+
+var clock = document.querySelector(".clock-text");
+
+function askTime() {
+  var date = new Date();
+  var hour = date.getHours();
+  var min = date.getMinutes();
+  var sec = date.getSeconds();
+  clock.innerText = `${hour < 10 ? `0${hour}` : hour}:${
+    min < 10 ? `0${min}` : min
+  }:${sec < 10 ? `0${sec}` : sec}`;
+}
+
+function init() {
+  askTime();
+  setInterval(askTime, 1000);
+}
+
+init();
